b030e5fa0501a55a0aaff2c2fc0310a2fed19b77
Jake <jake.arefyev@gmail.com>
2018-06-22 14:05:51 +0300

    format

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,42 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-const Tray = electron.Tray
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const {app, ipcMain, Tray, BrowserWindow} = electron;
 
-const path = require('path')
-const url = require('url')
-const {ipcMain} = require('electron')
+const path = require('path');
+const url = require('url');
 
-const settings = require('./settings')
-
+const settings = require('./settings');
 
 /**
-* Initialize an Electron application.
-*/
+ * Initialize an Electron application.
+ */
 class AppElectron {
+
     constructor() {
         // This method will be called when Electron has finished
         // initialization and is ready to create browser windows.
         // Some APIs can only be used after this event occurs.
-        app.on('ready', this.createWindow)
-        app.commandLine.appendSwitch('ignore-certificate-errors')
+        app.on('ready', this.createWindow);
+        app.commandLine.appendSwitch('ignore-certificate-errors');
         // Quit when all windows are closed.
-        app.on('window-all-closed', function() {
+        app.on('window-all-closed', function () {
             // On OS X it is common for applications and their menu bar
             // to stay active until the user quits explicitly with Cmd + Q
             if (process.platform !== 'darwin') {
-                app.quit()
+                app.quit();
             }
         })
 
-        app.on('activate', function() {
+        app.on('activate', function () {
             // On OS X it's common to re-create a window in the app when the
             // dock icon is clicked and there are no other windows open.
-            if (this.mainWindow === null) this.createWindow()
+            if (this.mainWindow === null) this.createWindow();
         })
     }
 
 
     createWindow() {
-        this.tray = new Tray(path.join(__dirname, 'img', 'electron-systray.png'))
-        this.tray.setToolTip(settings.name)
+        this.tray = new Tray(path.join(__dirname, 'img', 'electron-systray.png'));
+        this.tray.setToolTip(settings.name);
 
 
         // Create the browser window.
@@ -53,35 +48,33 @@ class AppElectron {
             show: false,
             title: settings.name,
             width: 500,
-        })
+        });
 
         ipcMain.on('resize-window', (event, data) => {
-            const currentSize = this.mainWindow.getContentSize()
+            const currentSize = this.mainWindow.getContentSize();
             if (data.height !== currentSize[1]) {
-                if (data.height > 600) data.height = 600
-                this.mainWindow.setSize(500, data.height, true)
+                if (data.height > 600) data.height = 600;
+                this.mainWindow.setSize(500, data.height, true);
             }
-        })
+        });
 
         // and load the index.html of the app.
         this.mainWindow.loadURL(url.format({
             pathname: path.join(__dirname, 'index.html'),
             protocol: 'file:',
             slashes: true,
-        }))
+        }));
 
-        this.mainWindow.once('ready-to-show', () => {
-            this.mainWindow.show()
-        })
+        this.mainWindow.once('ready-to-show', () => this.mainWindow.show());
 
         // Emitted when the window is closed.
-        this.mainWindow.on('closed', function() {
+        this.mainWindow.on('closed', function () {
             // Dereference the window object, usually you would store windows
             // in an array if your app supports multi windows, this is the time
             // when you should delete the corresponding element.
-            this.mainWindow = null
+            this.mainWindow = null;
         })
     }
 }
 
-global.app = new AppElectron()
+global.app = new AppElectron();
